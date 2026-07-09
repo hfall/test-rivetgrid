72ecd85da1221f820deddaad05dc6eee90e1f5ed
Hassane Fall <hassanef55@gmail.com>
2026-07-08 21:48:05 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="p-3">
+      <h1>Testing RivetGrid</h1>
+      <div className="">content here</div>
+    </div>
+  );
+}
+
+export default App;
